4f54394c0c68f3da1038ef0448d9905979021332
Sebb <sebastien.mande@outlook.fr>
2024-12-24 10:08:19 +0100

    modified gitignore
    remove .env line in gitignores

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,20 @@
-import { Roboto } from "next/font/google"
-import "@/app/globals.css"
+import { Roboto } from "next/font/google";
+import "@/app/globals.css";
 
 export const metadata = {
-  title: 'ToDo List',
-  description: 'ToDo List',
-}
+  title: "ToDo List",
+  description: "ToDo List",
+};
 
-const font = Roboto({ subsets : ['latin'], weight : ['100', '300', '400', '500', '700', '900']})
+const font = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={`${font.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
